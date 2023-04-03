9af9441347364908d00138d0f439bdf337745870
unknown <rv8828120@gmail.com>
2023-04-03 20:55:23 +0800

    First Commit

---
+++ assignment 2, number 2.py
@@ -0,0 +1,105 @@
+#Vargas, Ruben A.
+#BSCPE 1-4
+#Object-Oriented Programming
+#Assignment 2 - #2
+
+#Header design
+print("\033[;33;1;3m=\033[0m" * 70)
+print("\033[;1;3mHello, I hope you are doing good!\U0001f600\033[0m".center(81))
+print("\033[;33;1;3m=\033[0m" * 70)
+
+#Ask what is the name of the user and greetings
+print("")
+print("\033[1;3mMy name is \033[;96;1;3mCodeperman\033[0m")
+your_name = input("\033[1;3mWhat is your name?\033[0m")
+print("\033[;1;3mNice to meet you!\033[;34;1;3m" + your_name + "\033[0m \033[;1;3m, be ready!\033[0m")
+
+print("")
+print("\033[;33;1;3m-\033[0m" * 70)
+print("\033[;1;3mLet us start our mission for today!\033[0m".center(81))
+print("\033[;33;1;3m-\033[0m" * 70)
+
+#Ask the user to input the encrypted message
+encrypted_message = input(f"\n\033[33;1;3mType or input your encrypted message: \033[0m")
+decrypted_message = ""
+
+#Count the characters you entered
+characters_counter = len(encrypted_message)
+print(f"\n\033[1mThe count of the characters you have entered is", characters_counter)
+
+#Its time to turn the encrypted to decrypted message
+for encrypted in encrypted_message: 
+
+#   If this symbol "*" is/are in the message, change it to letter a
+    if "*" in encrypted:
+        decrypted_message += "a"
+
+#   If this symbol "&" is/are in the message, change it to letter e
+    elif "&" in encrypted:
+        decrypted_message += "e"
+
+#   If this symbol "#" is/are in the message, change it to letter i
+    elif "#" in encrypted:
+        decrypted_message += "i"
+
+#   If this symbol "+" is/are in the message, change it to letter o
+    elif "+" in encrypted:
+        decrypted_message += "o"
+
+#   If this symbol "!" is/are in the message, change it to letter u
+    elif "!" in encrypted:
+        decrypted_message += "u"
+    else:
+        decrypted_message += encrypted
+
+#Print the outcome
+print(f"\n\033[96;1;3mEncrypted Message: \033[0m" + encrypted_message)
+print("\033[96;1;3mDecrypted Message: \033[0m" + decrypted_message)
+
+print("")
+print("\033[;33;1;3mMission Accomplished!\033[0m".center(84, "~"))
+
+#Ask the user if they want to decrypt a message once again.
+while True:
+    print(f"\n\033[40m\033[1;3mDo you want to decrypt other message, again?\033[0m")
+    yes_or_no = input("\033[40m\033[1;3mType\033[40m\033[0m \033[40m\033[33;1;3mYES\033[0m \033[40m\033[1;3mif you want to continue and \033[40m\033[0m\033[33;1;3m\033[40mNO\033[0m \033[40m\033[1;3mif you want to exit. \033[0m")
+    if yes_or_no == "YES":
+    #Header design
+        print("")
+        print("\033[;36;1;3m=\033[0m" * 70)
+        print("\033[;1;3mHello, again!\U0001f600\033[0m".center(81))
+        print("\033[;36;1;3m=\033[0m" * 70)
+    #Ask the user to input the encrypted message
+        encrypted_message = input(f"\n\033[33;1;3mType or input your encrypted message: \033[0m")
+        decrypted_message = ""
+    #Count the characters you entered
+        characters_counter = len(encrypted_message)
+        print(f"\n\033[1mThe count of the characters you have entered is", characters_counter)
+    #Its time to turn the encrypted to decrypted message
+        for encrypted in encrypted_message: 
+    #   If this symbol "*" is/are in the message, change it to letter a
+            if "*" in encrypted:
+                decrypted_message += "a"
+    #   If this symbol "&" is/are in the message, change it to letter e
+            elif "&" in encrypted:
+                decrypted_message += "e"
+    #   If this symbol "#" is/are in the message, change it to letter i
+            elif "#" in encrypted:
+                decrypted_message += "i"
+    #   If this symbol "+" is/are in the message, change it to letter o
+            elif "+" in encrypted:
+                decrypted_message += "o"
+    #   If this symbol "!" is/are in the message, change it to letter u
+            elif "!" in encrypted:
+                decrypted_message += "u"
+            else:
+                decrypted_message += encrypted
+    #Print the outcome
+        print(f"\n\033[96;1;3mEncrypted Message: \033[0m" + encrypted_message)
+        print("\033[96;1;3mDecrypted Message: \033[0m" + decrypted_message)
+        print("")
+        print("\033[;33;1;3mMission Accomplished!\033[0m".center(84, "~"))
+
+    if yes_or_no == "NO":
+        print(f"\n\033[31;1;3mCodperman is leaving.....\033[0m")
+        break
---
+++ assignment 2, number 3.py
@@ -0,0 +1,94 @@
+#Vargas, Ruben A.
+#BSCPE 1-4
+#Object-Oriented Programming
+#Assignment 2 - #3
+
+#Header design
+print("\033[35m•\033[0m" * 58)
+print("~" + "\033[;96;1;3mCodeperman\033[0m \033[;1;3mis on duty!\033[0m".center(81) + "~")
+print("\033[35m•\033[0m" * 58)
+
+#Ask the name of the user
+user_name = input(f"\n\033[;33;1;3mWhat is your name? \033[0m")
+print(f"\n\033[;1;3mHey there, \033[;34;1;3m" + user_name +"\033[;1;3m!\033[0m")
+print("\033[0m\033[;35;1;3mPlease enter below the text that you want to encrypt!\033[0m")
+print("")
+print("\033[35m———\033[0m" * 19)
+
+#Create a python program using Vigenère Cipher to encrypt the message
+def vigenere_cipher(message, key):
+    #Convertion of message and key to their specific numerical numbers
+    convert_message = [ord(i) - 65 for i in message]
+    convert_key = [ord(i) - 65 for i in key]
+
+    #Encrypt the message using Vigenère Cipher
+    cipher = []
+    for i in range(len(convert_message)):
+        encrypted_value = (convert_message[i] + convert_key[i % len(key)]) % 26
+        cipher.append(encrypted_value)
+
+    #Ciphertext numerical numbers to letters
+    ciphertext = ''.join([chr(encrypted_value + 65) for encrypted_value in cipher])
+    return ciphertext
+
+#Ask the user to enter the message and key
+print("")
+message = input("\033[;33;1;3mYour message: \033[0m")
+message = message.upper()
+message = message.replace(" ","")
+key = input("\033[;33;1;3mYour key: \033[0m")
+key = key.upper()
+key = key.replace(" ","")
+
+#Print the outcome
+ciphertext = vigenere_cipher(message, key)
+print(f"\n\033[;1;3mWait a bit....\033[0m")
+print(f"\n\033[;32;1;3mAnd, done!\033[0m")
+print("")
+print(f"\n\033[;33;1;3mHere's the generated ciphertext:\033[0m", ciphertext)
+print("\033[35m———\033[0m" * 19)
+
+#Ask the user if they want to decrypt a text once again.
+while True:
+    print(f"\n\033[1;3mDo you want to try it again?\033[0m")
+    pick_one = input("\033[40m\033[33;1;3mYES\033[0m \033[1;3mor\033[0m \033[40m\033[0m\033[33;1;3m\033[40mNO\033[0m \033[1;3m? \033[0m")
+    if pick_one == "YES":
+        print(f"\n")
+        print("\033[32m•\033[0m" * 58)
+        print("~" + "\033[;96;1;3mCodeperman\033[0m \033[;1;3mis here!\033[0m".center(81) + "~")
+        print("\033[32m•\033[0m" * 58)
+        print("")
+        print("\033[0m\033[;32;1;3mPlease enter below the text that you want to encrypt!\033[0m")
+        print("")
+        print("\033[32m———\033[0m" * 19)
+#Create a python program using Vigenère Cipher to encrypt the message
+        def vigenere_cipher(message, key):
+    #Convertion of message and key to their specific numerical numbers
+            convert_message = [ord(i) - 65 for i in message]
+            convert_key = [ord(i) - 65 for i in key]
+    #Encrypt the message using Vigenère Cipher
+            cipher = []
+            for i in range(len(convert_message)):
+                encrypted_value = (convert_message[i] + convert_key[i % len(key)]) % 26
+                cipher.append(encrypted_value)
+    #Ciphertext numerical numbers to letters
+            ciphertext = ''.join([chr(encrypted_value + 65) for encrypted_value in cipher])
+            return ciphertext
+#Ask the user to enter the message and key
+        print("")
+        message = input("\033[;33;1;3mYour message: \033[0m")
+        message = message.upper()
+        message = message.replace(" ","")
+        key = input("\033[;33;1;3mYour key: \033[0m")
+        key = key.upper()
+        key = key.replace(" ","")
+#Print the outcome
+        ciphertext = vigenere_cipher(message, key)
+        print("")
+        print(f"\n\033[;33;1;3mHere's the generated ciphertext:\033[0m", ciphertext)
+        print("\033[32m———\033[0m" * 19)
+
+    if pick_one == "NO":
+        print("")
+        print(f"\n\033[31;1;3mCodperman is leaving.....\033[0m")
+        break
